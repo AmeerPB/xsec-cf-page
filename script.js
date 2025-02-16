b8b946a10ba58d2af31f3f3fb91a069af0ef756d
ameerpb <ameerpbabu@gmail.com>
2025-02-16 16:18:26 +0530

    Updated index script and style

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ const slider = document.querySelector('.slider');
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+  if (e.target.matches('.next') || e.keyCode === 39) {
+    slider.append(items[0]);
+  } else if (e.target.matches('.prev') || e.keyCode === 37) {
+    slider.prepend(items[items.length - 1]);
+  }
 }
 
-document.addEventListener('click',activate,false);
+document.addEventListener('click', activate, false);
+window.addEventListener('keyup', arrowUp);
+window.addEventListener('keydown', arrowDown);
+window.addEventListener('keydown', activate); // Add key event for slider navigation
+
+function arrowDown(e) {
+  const key = document.querySelector(`.arrow-key[data-key="${e.keyCode}"]`);
+  if (key) key.classList.add('press');
+}
+
+function arrowUp(e) {
+  const key = document.querySelector(`.arrow-key[data-key="${e.keyCode}"]`);
+  if (key) key.classList.remove('press');
+}
